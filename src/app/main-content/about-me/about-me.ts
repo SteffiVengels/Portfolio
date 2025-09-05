2bf2cc2dca46c23f69618b070ec28a67ddb320bb
Steffi Vengels <vengels.stefanie@gmail.com>
2025-09-05 18:43:32 +0200

    finished about me section

--- src/app/main-content/about-me/about-me.ts
+++ src/app/main-content/about-me/about-me.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-about-me',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe],
   templateUrl: './about-me.html',
   styleUrl: './about-me.scss'
 })
 export class AboutMe {
-
+    constructor(public translate: TranslateService) {
+  }
 }

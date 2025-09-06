b572c68274fc67ef39e5e4608ee0633c7530d2c6
Steffi Vengels <vengels.stefanie@gmail.com>
2025-09-06 16:14:04 +0200

    finished skill section

--- src/app/main-content/skills/skills.ts
+++ src/app/main-content/skills/skills.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skills',
   standalone: true,
-  imports: [],
+  imports: [TranslatePipe, CommonModule],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
 export class Skills {
+
+  skilllist = [
+    { name: 'HTML', img: 'html.png' },
+    { name: 'CSS', img: 'css.png' },
+    { name: 'JavaScript', img: 'js.png' },
+    { name: 'Material Design', img: 'materialdesign.png' },
+    { name: 'TypeScript', img: 'ts.png' },
+    { name: 'Angular', img: 'angular.png' },
+    { name: 'Firebase', img: 'firebase.png' },
+    { name: 'Git', img: 'git.png' },
+    { name: 'REST-API', img: 'rest-api.png' },
+    { name: 'Scrum', img: 'scrum.png' }
+  ]
+  constructor(public translate: TranslateService) {
+  }
 
 }

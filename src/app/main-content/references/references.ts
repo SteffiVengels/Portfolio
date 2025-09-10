840db9b75b07eddbcc01964c3d5463fd79f12830
Steffi Vengels <vengels.stefanie@gmail.com>
2025-09-10 18:59:39 +0200

    start with animation

--- src/app/main-content/references/references.ts
+++ src/app/main-content/references/references.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-references',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TranslatePipe],
   templateUrl: './references.html',
   styleUrl: './references.scss'
 })
@@ -15,21 +17,21 @@ export class References {
       id: 1,
       name: 'H. Janisch',
       role: 'Team Partner',
-      text: 'Lukas has proven to be a reliable group partner. His technical skills and proactive approach were crucial to the success of our project. '
     },
     {
       id: 2,
       name: 'A. Fischer',
       role: 'Team Partner',
-      text: 'I had the good fortune of working with Lukas in a group project at the Developer Akademie that involved a lot of effort. He always stayed calm, cool, and focused, and made sure our team was set up for success. He is super knowledgeable, easy to work with, and I\'d happily work with him again given the chance.'
     },
     {
-      id: 3,  
+      id: 3,
       name: 'T. Schulz',
       role: 'Frontend Developer',
-      text: 'Our project benefited enormously from Simon efficient way of working.'
     }
   ];
+
+  constructor(public translate: TranslateService) {
+  }
 
   currentFeedbackIndex = 0;
 
@@ -40,4 +42,14 @@ export class References {
     this.currentFeedbackIndex = (this.currentFeedbackIndex - 1 + this.feedbackList.length) % this.feedbackList.length;
   }
 
+  getVisibleFeedback() {
+    const prevIndex = (this.currentFeedbackIndex - 1 + this.feedbackList.length) % this.feedbackList.length;
+    const nextIndex = (this.currentFeedbackIndex + 1) % this.feedbackList.length;
+
+    return [
+      this.feedbackList[prevIndex],
+      this.feedbackList[this.currentFeedbackIndex],
+      this.feedbackList[nextIndex]
+    ];
+  }
 }

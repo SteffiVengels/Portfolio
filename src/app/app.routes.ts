384b117d4c31b6351fa77197e4e946222af2aec3
Steffi Vengels <vengels.stefanie@gmail.com>
2025-09-14 15:21:31 +0200

    add privacy policy

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainContent } from './main-content/main-content';
 import { LegalNotice } from './legal-notice/legal-notice';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
 
 export const routes: Routes = [
     { path: '', component: MainContent},
-    { path: 'legal-notice', component: LegalNotice}
+    { path: 'legal-notice', component: LegalNotice},
+    { path: 'privacy-policy', component: PrivacyPolicy}
 ];
